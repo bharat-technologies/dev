4521b44cccd6858bd857ce8fc6e502ebf8466dd9
Udhaykrishn <krishnauday320@gmail.com>
2025-04-01 14:03:56 +0530

    feat: for all middleware and routes setup file

--- 1.0.1/backend/src/app.ts
+++ 1.0.1/backend/src/app.ts
@@ -1,19 +1,26 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express'
+import compression from "compression"
+import cors from "cors"
+import { config } from 'dotenv'
+config()
+import formRouter from "@/routes/formRouter"
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-// Middleware setup
+app.use(cors())
+app.use(compression())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Routes setup
-import routes from './routes';
-app.use('/api', routes);
+app.use("/v1/api/", formRouter)
 
-// Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+    res.status(error.status || 500).json({
+        success: false,
+        message: error.message || "Internal Server Error",
+    });
 });
+
 
 export default app;
